a4995c432330eba9ec3a8b01733ac43abf341722
Jakub Chabera <jakub.chabera.021@pslib.cz>
2022-12-04 22:29:01 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Bojovnik
+    {
+        private string _jmeno;
+
+        public int HP { get; private set; }
+
+        public Bojovnik(string jmeno)
+        {
+
+            _jmeno = jmeno;
+            HP = 100;
+        }
+        public Bojovnik(string jmeno, int HP)
+        {
+
+            _jmeno = jmeno;
+            this.HP = HP;
+        }
+
+        public string GetJmeno()
+        {
+
+            return _jmeno;
+        }
+
+        public void SetJmeno(String jmeno)
+        {
+            if (jmeno != "")
+            {
+                _jmeno = jmeno;
+            }
+        }
+
+        public bool getHit(int damage)
+        {
+
+            if (damage > 0)
+            {
+
+                HP = HP - damage;
+                if (HP < 0)
+                {
+                    HP = 0;
+                }
+            }
+            return HP > 0;
+        }
+
+        public bool IsAlive /* jestli bojovnik zije */
+        {
+            get
+            {
+                if (HP > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public string Jmeno
+        {
+            get { return _jmeno; }
+            set { _jmeno = value; }
+
+        }
+
+
+        public void AttackFighter(Bojovnik target, bool screaming = false)
+        {
+
+            int damage;
+
+            if (screaming)
+            {
+                damage = Random.Shared.Next(10, 60);
+            }
+            else
+            {
+                damage = Random.Shared.Next(0, 50);
+            }
+            target.getHit(damage);
+
+        }
+
+        public void AttackKnight(Bojovnik target, bool screaming = false)
+        {
+            int damage;
+
+            if (screaming)
+            {
+                damage = Random.Shared.Next(10, 40);
+            }
+            else
+            {
+                damage = Random.Shared.Next(0, 40);
+            }
+            target.getHit(damage);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using ConsoleApp2;
+
+
+Bojovnik Jakub = new Bojovnik("Jakub");
+Bojovnik Enemy = new Bojovnik("Enemy", 100);
+Bojovnik Knight = new Bojovnik("Knight", 130);
+Enemy.Jmeno = "Enemy";
+
+Console.WriteLine(Jakub.GetJmeno());
+Console.WriteLine(Jakub.HP + " HP");
+Console.WriteLine("");
+Console.Write("S kým chceš bojovat?");
+string userChoice = Console.ReadLine();
+
+/*Console.WriteLine("Je " + Jakub.Jmeno + " naživu ? " + Jakub.IsAlive); */
+
+
+
+if (userChoice == "enemy")
+{
+    Jakub.AttackFighter(Enemy);
+    Enemy.AttackFighter(Jakub, true);
+
+    Console.WriteLine(Jakub.Jmeno + " má " + Jakub.HP + " HP");
+    Console.WriteLine(Enemy.Jmeno + " má " + Enemy.HP + " HP");
+
+    bool BojovnikAttackFirst;
+    if (Random.Shared.Next(0, 1) == 1)
+    {
+
+    }
+}
+else if (userChoice == "knight")
+{
+    Jakub.AttackKnight(Knight);
+    Knight.AttackKnight(Jakub, true);
+
+    Console.WriteLine(Jakub.Jmeno + " má " + Jakub.HP + " HP");
+    Console.WriteLine(Knight.Jmeno + " má " + Knight.HP + " HP");
+
+    bool BojovnikAttackKnight;
+    if (Random.Shared.Next(0, 1) == 1)
+    {
+
+    }
+
+    Console.Write("Chceš dohrát boj? ");
+    string vyber = Console.ReadLine();
+
+    if (vyber == "ano")
+    {
+
+    }
+    else
+    {
+        if (Jakub.HP > Knight.HP)
+        {
+            Console.WriteLine(Jakub.Jmeno + " vyhrál!");
+        }
+        else if (Jakub.HP < Knight.HP)
+        {
+            Console.WriteLine(Jakub.Jmeno + " prohál :(");
+        }
+    }
+}
+
+
+
+
+
+
+
+
